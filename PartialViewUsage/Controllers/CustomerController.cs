8578409cd0559f63ad9fd51e580bea438a838e77
saliha <salihagokce91@gmail.com>
2020-12-30 01:37:35 +0300

    first commit

---
+++ PartialViewUsage/Controllers/CustomerController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartialViewUsage.Models;
+
+namespace PartialViewUsage.Controllers
+{
+    public class CustomerController : Controller
+    {
+        //[HttpGet]
+        public List<Customer> customerList = new List<Customer>();
+        public IActionResult Index()
+        {
+
+            var cust1 = new Customer();
+            cust1.Name = "Name1";
+            cust1.Surname = "Surname1";
+            cust1.Age = 23;
+            cust1.Phone = "234544";
+            cust1.Address = "istanbul";
+            cust1.CustomerStatus = true;
+            customerList.Add(cust1);
+
+            var cust2 = new Customer();
+            cust2.Name = "Name2";
+            cust2.Surname = "Surname2";
+            cust2.Age = 44;
+            cust2.Phone = "5557776654";
+            cust2.Address = "istanbul/Beyoğlu";
+            cust2.CustomerStatus = false;
+            customerList.Add(cust2);
+            return View(customerList);
+        }
+        //public ActionResult getCustomer()
+        //{
+
+        //}
+        public ActionResult addCustomer(Customer customer)
+        {
+            Customer cust3 = new Customer();
+            cust3.Name = customer.Name;
+            cust3.Surname= customer.Surname;
+            cust3.Age = customer.Age;
+            cust3.Address = customer.Address;
+            cust3.Phone = customer.Phone;
+            cust3.CustomerStatus = true;
+            customerList.Add(cust3);
+            return View(customer);
+        }
+    }
+}
---
+++ PartialViewUsage/Models/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PartialViewUsage.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public bool CustomerStatus { get; set; }
+    }
+}
